bbc92487be7be81ae3dbaa01ced68cc4bf976687
Iván Szkiba <szkiba@users.noreply.github.com>
2026-05-05 08:37:31 +0200

    Update k6 dependency to v2.0.0-rc1 (#82)

--- redis/client_test.go
+++ redis/client_test.go
@@ -11,11 +11,11 @@ import (
 	"github.com/grafana/sobek"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"go.k6.io/k6/js/modulestest"
-	"go.k6.io/k6/lib"
-	"go.k6.io/k6/lib/netext"
-	"go.k6.io/k6/lib/types"
-	"go.k6.io/k6/metrics"
+	"go.k6.io/k6/v2/js/modulestest"
+	"go.k6.io/k6/v2/lib"
+	"go.k6.io/k6/v2/lib/netext"
+	"go.k6.io/k6/v2/lib/types"
+	"go.k6.io/k6/v2/metrics"
 )
 
 func TestClientConstructor(t *testing.T) {
